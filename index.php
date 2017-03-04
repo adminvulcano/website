496659d2945138b79b6d0c59bc7fa0d705347d4d
adminvulcano <derwolf8@outlook.de>
2017-03-04 10:40:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+
+<html lang="de">
+<head>
+  <?php include 'include/head.php'; ?>
+  <meta name="description" content="
+  Profitieren Sie von der grossen Auswahl an Phyton-, Blender-, oder
+  Unityprogrammen sowie von freiem Speicher für Ihre Photos und Skripte
+  ">
+</head>
+<body>
+<title>Übersicht - Vulcano</title>
+<!-- Navigation -->
+<?php include 'cookieauth.php'
+ ?>
+<!-- Anzuzeigender Inhalt -->
+<div class="inhalt">
+<div class="home" id="home">
+<h1>Willkommen <hr> bei Vulcano</h1>
+</div>
+<!-- Einleitung -->
+<div class="about" id="about">
+  <section>
+<h2>wer wir sind</h2>
+<p>
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</section>
+<!-- Button um auf der Mobileversion das Overflow anzuzeigen -->
+<button href="#" class="readmore">read more...</button>
+</div>
+<!-- Download für irgendwas -->
+<div class="download" id="download">
+<section>
+<br><h2>Unsere Programme</h2><br>
+<p>Geniesse unsere neuesten <br> Innovationen mit<br>
+  <a href="downloads/Demo.zip" download="Demo.zip" title="Liebevoll mit Phyton programmiert">
+     Phyton<br><i  class="fa fa-download fa-4x phyton" ></i>
+  </a>
+  <br>
+  <a href="downloads/onlinetest.zip" download="unity.zip" title="Liebevoll mit Unity programmiert">
+     Unity<br><i  class="fa fa-download fa-4x unity" ></i>
+  </a>
+  <br>
+  oder
+  <br>
+  <a href="downloads/rubik.zip" download="Blender.zip" title="Liebevoll mit Blender programmiert">
+     Blender<br><i  class="fa fa-download fa-4x blender" ></i>
+  </a>
+</p>
+</section>
+</div>
+<!-- Platzhalter -->
+<div class="addition" id="addition">
+  <section>
+  <h2>Einige Dinge...</h2>
+<br> <p>
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</section>
+<!-- Button um auf der Mobileversion den Overflow anzuzeigen -->
+<button href="#" class="readmore">read more...</button>
+</div>
+<?php include 'include/footer.php'; ?>
+</div>
+<!-- google-analytics -->
+
+<?php
+$options = "true,true,true,false,true";
+require 'include/jsloader.php';
+ ?>
+</body>
+
+</html>
